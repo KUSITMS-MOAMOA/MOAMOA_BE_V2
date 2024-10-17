5a089adf18c8de545e1ea828388999b0dbe3b302
oosedus <seayeon0729@gmail.com>
2024-10-18 01:18:46 +0900

    style : errorStatus 응답 code 수정

--- src/main/java/corecord/dev/domain/user/exception/enums/UserErrorStatus.java
+++ src/main/java/corecord/dev/domain/user/exception/enums/UserErrorStatus.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum UserErrorStatus implements BaseErrorStatus {
-    INVALID_USER_NICKNAME(HttpStatus.BAD_REQUEST, "E001", "유효하지 않은 닉네임입니다."),
-    INVALID_USER_STATUS(HttpStatus.BAD_REQUEST, "E002", "신분상태의 입력이 잘못되었습니다.");
+    INVALID_USER_NICKNAME(HttpStatus.BAD_REQUEST, "E101_NICKNAME", "유효하지 않은 닉네임입니다."),
+    INVALID_USER_STATUS(HttpStatus.BAD_REQUEST, "E101_STATUS", "신분상태의 입력이 잘못되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
