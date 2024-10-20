fe2c739186cc80ff44ea1a5fc7929b0ed1184815
oosedus <seayeon0729@gmail.com>
2024-10-21 00:25:53 +0900

    fix : 로그인 성공 로직 수정

--- src/main/java/corecord/dev/domain/auth/application/OAuthLoginSuccessHandler.java
+++ src/main/java/corecord/dev/domain/auth/application/OAuthLoginSuccessHandler.java
@@ -5,7 +5,9 @@ import corecord.dev.common.util.JwtUtil;
 import corecord.dev.domain.auth.dto.KakaoUserInfo;
 import corecord.dev.domain.auth.dto.OAuth2UserInfo;
 import corecord.dev.domain.token.entity.RefreshToken;
+import corecord.dev.domain.token.entity.TmpToken;
 import corecord.dev.domain.token.repository.RefreshTokenRepository;
+import corecord.dev.domain.token.repository.TmpTokenRepository;
 import corecord.dev.domain.user.entity.User;
 import corecord.dev.domain.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
@@ -36,6 +38,7 @@ public class OAuthLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHand
     private final CookieUtil cookieUtil;
     private final UserRepository userRepository;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final TmpTokenRepository tmpTokenRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
@@ -58,65 +61,34 @@ public class OAuthLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHand
 
     // 기존 유저 처리
     private void handleExistingUser(HttpServletRequest request, HttpServletResponse response, User user) throws IOException {
-        log.info("기존 유저입니다. 액세스 토큰과 리프레쉬 토큰을 발급합니다.");
+        log.info("기존 유저입니다. 임시 토큰을 발급합니다.");
+        // 기존 리프레쉬 토큰, 쿠키 삭제
+        deleteByUserId(user.getUserId());
+        response.addCookie(cookieUtil.deleteCookie("refreshToken"));
 
-        // 기존 리프레쉬 토큰 삭제
-        refreshTokenRepository.deleteByUserId(user.getUserId());
+        // 임시 토큰 생성
+        String tmpToken = jwtUtil.generateTmpToken(user.getUserId());
+        tmpTokenRepository.save(TmpToken.of(tmpToken, user.getUserId()));
+        String redirectURI = String.format(ACCESS_TOKEN_REDIRECT_URI, tmpToken);
 
-        // 새로운 리프레쉬 토큰 생성 및 저장
-        String refreshToken = jwtUtil.generateRefreshToken(user.getUserId());
-        saveRefreshToken(user, refreshToken);
-
-        // 기존 쿠키 삭제
-        deleteExistingTokens(response);
-
-        // RefreshToken 쿠키 추가
-        ResponseCookie refreshTokenCookie = cookieUtil.createTokenCookie("refreshToken", refreshToken);
-        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
-
-        // AccessToken 쿠키 추가
-        String accessToken = jwtUtil.generateAccessToken(user.getUserId());
-        ResponseCookie accessTokenCookie = cookieUtil.createTokenCookie("accessToken", accessToken);
-        response.addHeader("Set-Cookie", accessTokenCookie.toString());
-
-        String redirectURI = String.format(ACCESS_TOKEN_REDIRECT_URI, accessToken, refreshToken);
-
-        // 액세스 토큰 리다이렉트
         getRedirectStrategy().sendRedirect(request, response, redirectURI);
     }
 
     // 신규 유저 처리
     private void handleNewUser(HttpServletRequest request, HttpServletResponse response, String providerId) throws IOException {
         log.info("신규 유저입니다. 레지스터 토큰을 발급합니다.");
-
-        // 이미 레지스터 토큰이 있다면 삭제
-        response.addCookie(cookieUtil.deleteCookie("registerToken"));
-
         // 레지스터 토큰 발급
         String registerToken = jwtUtil.generateRegisterToken(providerId);
-
-        // 레지스터 토큰 쿠키 설정
-        ResponseCookie registerTokenCookie = cookieUtil.createTokenCookie("registerToken", registerToken);
-        response.addHeader("Set-Cookie", registerTokenCookie.toString());
-
         String redirectURI = String.format(REGISTER_TOKEN_REDIRECT_URI, registerToken);
-
-        // 레지스터 토큰 리다이렉트
         getRedirectStrategy().sendRedirect(request, response, redirectURI);
     }
 
-    // 리프레쉬 토큰 저장
-    private void saveRefreshToken(User user, String refreshToken) {
-        RefreshToken newRefreshToken = RefreshToken.builder()
-                .userId(user.getUserId())
-                .refreshToken(refreshToken)
-                .build();
-        refreshTokenRepository.save(newRefreshToken);
-    }
-
-    // 기존 토큰 삭제
-    private void deleteExistingTokens(HttpServletResponse response) {
-        response.addCookie(cookieUtil.deleteCookie("accessToken"));
-        response.addCookie(cookieUtil.deleteCookie("refreshToken"));
+    private void deleteByUserId(Long userId) {
+        Iterable<RefreshToken> tokens = refreshTokenRepository.findAll();
+        for (RefreshToken token : tokens) {
+            if (token.getUserId().equals(userId)) {
+                refreshTokenRepository.delete(token);
+            }
+        }
     }
 }
