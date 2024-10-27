2255aaa26e91590030d1cfb43fd10e87131e7bc8
oosedus <seayeon0729@gmail.com>
2024-10-27 16:46:05 +0900

    feat : 로그아웃 기능 구현

--- src/main/java/corecord/dev/domain/user/constant/UserSuccessStatus.java
+++ src/main/java/corecord/dev/domain/user/constant/UserSuccessStatus.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum UserSuccessStatus implements BaseSuccessStatus {
 
-    USER_REGISTER_SUCCESS(HttpStatus.CREATED, "S101", "회원가입이 성공적으로 완료되었습니다.");
+    USER_REGISTER_SUCCESS(HttpStatus.CREATED, "S101", "회원가입이 성공적으로 완료되었습니다."),
+    USER_LOGOUT_SUCCESS(HttpStatus.OK, "S801", "로그아웃이 성공적으로 완료되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
