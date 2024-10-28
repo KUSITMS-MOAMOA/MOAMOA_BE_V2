a391e1ea850cf7999e6cd59fc433304f24d973bc
daeun084 <030804jk@naver.com>
2024-10-28 18:28:06 +0900

    chore: folder 기능에 userId 추가

--- src/main/java/corecord/dev/domain/folder/controller/FolderController.java
+++ src/main/java/corecord/dev/domain/folder/controller/FolderController.java
@@ -1,6 +1,7 @@
 package corecord.dev.domain.folder.controller;
 
 import corecord.dev.common.response.ApiResponse;
+import corecord.dev.common.web.UserId;
 import corecord.dev.domain.folder.constant.FolderSuccessStatus;
 import corecord.dev.domain.folder.dto.request.FolderRequest;
 import corecord.dev.domain.folder.dto.response.FolderResponse;
@@ -17,36 +18,36 @@ public class FolderController {
 
     @PostMapping("")
     public ResponseEntity<ApiResponse<FolderResponse.FolderDtoList>> createFolder(
+            @UserId Long userId,
             @RequestBody FolderRequest.FolderDto folderDto
     ) {
-        FolderResponse.FolderDtoList folderResponse = folderService.createFolder(folderDto);
-
+        FolderResponse.FolderDtoList folderResponse = folderService.createFolder(userId, folderDto);
         return ApiResponse.success(FolderSuccessStatus.FOLDER_CREATE_SUCCESS, folderResponse);
     }
 
     @DeleteMapping("/{folderId}")
     public ResponseEntity<ApiResponse<FolderResponse.FolderDtoList>> deleteFolder(
+            @UserId Long userId,
             @PathVariable(name = "folderId") Long folderId
     ) {
-        FolderResponse.FolderDtoList folderResponse = folderService.deleteFolder(folderId);
-
+        FolderResponse.FolderDtoList folderResponse = folderService.deleteFolder(userId, folderId);
         return ApiResponse.success(FolderSuccessStatus.FOLDER_DELETE_SUCCESS, folderResponse);
     }
 
     @GetMapping("")
     public ResponseEntity<ApiResponse<FolderResponse.FolderDtoList>> getFolders(
+            @UserId Long userId
     ) {
-        FolderResponse.FolderDtoList folderResponse = folderService.getFolderList();
-
+        FolderResponse.FolderDtoList folderResponse = folderService.getFolderList(userId);
         return ApiResponse.success(FolderSuccessStatus.FOLDER_GET_SUCCESS, folderResponse);
     }
 
     @PatchMapping("")
     public ResponseEntity<ApiResponse<FolderResponse.FolderDtoList>> updateFolder(
+            @UserId Long userId,
             @RequestBody FolderRequest.FolderUpdateDto folderDto
     ) {
-        FolderResponse.FolderDtoList folderResponse = folderService.updateFolder(folderDto);
-
+        FolderResponse.FolderDtoList folderResponse = folderService.updateFolder(userId, folderDto);
         return ApiResponse.success(FolderSuccessStatus.FOLDER_UPDATE_SUCCESS, folderResponse);
     }
 
--- src/main/java/corecord/dev/domain/folder/exception/enums/FolderErrorStatus.java
+++ src/main/java/corecord/dev/domain/folder/exception/enums/FolderErrorStatus.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum FolderErrorStatus implements BaseErrorStatus {
     DUPLICATED_FOLDER_TITLE(HttpStatus.BAD_REQUEST, "E0400_DUPLICATED_TITLE", "이미 존재하는 폴더 명입니다."),
     OVERFLOW_FOLDER_TITLE(HttpStatus.BAD_REQUEST, "E0400_OVERFLOW_TITLE", "폴더 명은 15자 이내여야 합니다."),
+    USER_FOLDER_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "E401_FOLDER_UNAUTHORIZED", "유저가 폴더에 대한 권한이 없습니다."),
     FOLDER_NOT_FOUND(HttpStatus.NOT_FOUND, "E0404_FOLDER", "존재하지 않는 폴더입니다.");
 
     private final HttpStatus httpStatus;
