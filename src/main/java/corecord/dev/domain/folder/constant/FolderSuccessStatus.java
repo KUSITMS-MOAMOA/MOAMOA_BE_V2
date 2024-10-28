3be6af0524c684c5b57435f304416883fb9f35cc
daeun084 <030804jk@naver.com>
2024-10-28 20:49:56 +0900

    chore: folder success status 수정

--- src/main/java/corecord/dev/domain/folder/constant/FolderSuccessStatus.java
+++ src/main/java/corecord/dev/domain/folder/constant/FolderSuccessStatus.java
@@ -10,9 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum FolderSuccessStatus implements BaseSuccessStatus {
 
     FOLDER_CREATE_SUCCESS(HttpStatus.CREATED, "S601", "폴더 생성이 성공적으로 완료되었습니다."),
-    FOLDER_DELETE_SUCCESS(HttpStatus.OK, "S601", "폴더 삭제 성공적으로 완료되었습니다."),
-    FOLDER_GET_SUCCESS(HttpStatus.OK, "S601", "폴더 리스트 조회가 성공적으로 완료되었습니다."),
-    FOLDER_UPDATE_SUCCESS(HttpStatus.OK, "S601", "폴더 수정이 성공적으로 완료되었습니다.");
+    FOLDER_DELETE_SUCCESS(HttpStatus.OK, "S605", "폴더 삭제가 성공적으로 완료되었습니다."),
+    FOLDER_GET_SUCCESS(HttpStatus.OK, "S603", "폴더 리스트 조회가 성공적으로 완료되었습니다."),
+    FOLDER_UPDATE_SUCCESS(HttpStatus.OK, "S604", "폴더명 수정이 성공적으로 완료되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
