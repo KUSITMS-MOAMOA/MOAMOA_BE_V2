d44a79dfac5a5329941aa830e907e19a8b62618a
daeun084 <030804jk@naver.com>
2024-10-30 00:00:49 +0900

    add: 역량 키워드 추가

--- src/main/java/corecord/dev/domain/analysis/constant/Keyword.java
+++ src/main/java/corecord/dev/domain/analysis/constant/Keyword.java
@@ -4,9 +4,16 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum Keyword {
-    COMMUNICATION("커뮤니케이션"),
-    TEAMWORK("팀워크"),
-    LEADERSHIP("리더십")
+    PROBLEM_SOLViNG_SKILL("문제해결능력"), ANALYTICAL_SKILL("분석력"),
+    GLOBAL_SKILL("글로벌역량"), JUDGEMENT_SKILL("판단력"),
+
+    TARGET_AWARENESS("목표의식"), MOMENTUM("추진력"),
+
+    COMMUNICATION("커뮤니케이션"), LEADERSHIP("리더십"),
+    COLLABORATION("협업능력"), ADAPTABILITY("적응력"),
+
+    CREATIVITY("창의성"), LOGIC("논리성"),
+    CHALLENGE_MINDSET("도전정신"), SELF_IMPROVEMENT("자기계발"), RESPONSIBILITY("책임감")
     ;
 
     private final String value;
