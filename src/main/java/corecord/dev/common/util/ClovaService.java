51e0c04fe8fe3fb25a3412beffc2f782f4bbbd76
oosedus <seayeon0729@gmail.com>
2024-11-08 16:30:26 +0900

    refactor : ncp AI 호출 비동기 로직으로 변경

--- src/main/java/corecord/dev/common/util/ClovaService.java
+++ src/main/java/corecord/dev/common/util/ClovaService.java
@@ -6,11 +6,11 @@ import corecord.dev.common.exception.GeneralException;
 import corecord.dev.common.status.ErrorStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientException;
+import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
@@ -18,61 +18,38 @@ import org.springframework.web.reactive.function.client.WebClientException;
 public class ClovaService {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final WebClient webClient = WebClient.create();
+    private final WebClient webClient;
 
-    @Value("${ncp.chat.host}")
-    private String chatHost;
+    public Mono<String> generateAiResponse(ClovaRequest clovaRequest) {
+        log.info("AI 요청: {}", clovaRequest.getMessages());
 
-    @Value("${ncp.chat.api-key}")
-    private String chatApiKey;
-
-    @Value("${ncp.chat.api-key-primary-val}")
-    private String chatApiKeyPrimaryVal;
-
-    @Value("${ncp.chat.request-id}")
-    private String chatRequestId;
-
-    public String generateAiResponse(ClovaRequest clovaRequest) {
-        try {
-            log.info("AI 요청: {}", clovaRequest.getMessages());
-            String responseBody = webClient.post()
-                    .uri(chatHost)
-                    .header("X-NCP-CLOVASTUDIO-API-KEY", chatApiKey)
-                    .header("X-NCP-APIGW-API-KEY", chatApiKeyPrimaryVal)
-                    .header("X-NCP-CLOVASTUDIO-REQUEST-ID", chatRequestId)
-                    .header("Content-Type", "application/json; charset=utf-8")
-                    .header("Accept", "application/json")
-                    .bodyValue(clovaRequest)
-                    .retrieve()
-                    .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
-                        log.error("클라이언트 오류 발생: 상태 코드 - {}", clientResponse.statusCode());
-                        return clientResponse.bodyToMono(String.class)
-                                .map(errorBody -> new GeneralException(ErrorStatus.AI_CLIENT_ERROR));
-                    })
-                    .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
-                        log.error("서버 오류 발생: 상태 코드 - {}", clientResponse.statusCode());
-                        return clientResponse.bodyToMono(String.class)
-                                .map(errorBody -> new GeneralException(ErrorStatus.AI_SERVER_ERROR));
-                    })
-                    .bodyToMono(String.class)
-                    .block();
-
-            log.info("AI 응답: {}", responseBody);
-            return parseContentFromResponse(responseBody);
-        } catch (WebClientException e) {
-            log.error("채팅 AI 응답 생성 실패", e);
-            throw new GeneralException(ErrorStatus.AI_RESPONSE_ERROR);
-        }
+        return webClient.post()
+                .bodyValue(clovaRequest)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, this::handleClientError)
+                .onStatus(HttpStatusCode::is5xxServerError, this::handleServerError)
+                .bodyToMono(String.class)
+                .flatMap(this::parseContentFromResponse)
+                .doOnError(e -> log.error("채팅 AI 응답 생성 실패", e));
     }
 
-    private String parseContentFromResponse(String responseBody) {
-        try {
-            JsonNode root = objectMapper.readTree(responseBody);
-            JsonNode messageContent = root.path("result").path("message").path("content");
-            return messageContent.asText();
-        } catch (Exception e) {
-            log.error("응답 파싱 실패", e);
-            throw new GeneralException(ErrorStatus.AI_RESPONSE_ERROR);
-        }
+    private Mono<String> parseContentFromResponse(String responseBody) {
+        return Mono.fromCallable(() -> {
+                    JsonNode root = objectMapper.readTree(responseBody);
+                    return root.path("result").path("message").path("content").asText();
+                }).doOnError(e -> log.error("응답 파싱 실패", e))
+                .onErrorMap(e -> new GeneralException(ErrorStatus.AI_RESPONSE_ERROR));
+    }
+
+    private Mono<Throwable> handleClientError(ClientResponse clientResponse) {
+        log.error("클라이언트 오류 발생: 상태 코드 - {}", clientResponse.statusCode());
+        return clientResponse.bodyToMono(String.class)
+                .flatMap(errorBody -> Mono.error(new GeneralException(ErrorStatus.AI_CLIENT_ERROR)));
+    }
+
+    private Mono<Throwable> handleServerError(ClientResponse clientResponse) {
+        log.error("서버 오류 발생: 상태 코드 - {}", clientResponse.statusCode());
+        return clientResponse.bodyToMono(String.class)
+                .flatMap(errorBody -> Mono.error(new GeneralException(ErrorStatus.AI_SERVER_ERROR)));
     }
 }
