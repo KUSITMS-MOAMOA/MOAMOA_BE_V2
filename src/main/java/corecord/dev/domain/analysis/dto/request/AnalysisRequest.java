200de0a7cf41efa6874e20c7899f2c44a59e3e28
daeun084 <030804jk@naver.com>
2024-11-19 15:40:48 +0900

    test: analysis service test 작성

--- src/main/java/corecord/dev/domain/analysis/dto/request/AnalysisRequest.java
+++ src/main/java/corecord/dev/domain/analysis/dto/request/AnalysisRequest.java
@@ -1,13 +1,14 @@
 package corecord.dev.domain.analysis.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Map;
 
 public class AnalysisRequest {
 
-    @Data
+    @Data @Builder
     public static class AnalysisUpdateDto {
         @NotNull(message = "역량 분석 id를 입력해주세요.")
         private Long analysisId;
--- src/test/java/corecord/dev/analysis/service/AnalysisServiceTest.java
+++ src/test/java/corecord/dev/analysis/service/AnalysisServiceTest.java
@@ -2,8 +2,12 @@ package corecord.dev.analysis.service;
 
 import corecord.dev.domain.ability.entity.Ability;
 import corecord.dev.domain.ability.entity.Keyword;
+import corecord.dev.domain.ability.exception.enums.AbilityErrorStatus;
+import corecord.dev.domain.ability.exception.model.AbilityException;
 import corecord.dev.domain.ability.service.AbilityService;
+import corecord.dev.domain.analysis.dto.request.AnalysisRequest;
 import corecord.dev.domain.analysis.dto.response.AnalysisAiResponse;
+import corecord.dev.domain.analysis.dto.response.AnalysisResponse;
 import corecord.dev.domain.analysis.entity.Analysis;
 import corecord.dev.domain.analysis.exception.enums.AnalysisErrorStatus;
 import corecord.dev.domain.analysis.exception.model.AnalysisException;
@@ -15,6 +19,7 @@ import corecord.dev.domain.record.constant.RecordType;
 import corecord.dev.domain.record.entity.Record;
 import corecord.dev.domain.user.entity.Status;
 import corecord.dev.domain.user.entity.User;
+import corecord.dev.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,8 +28,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -34,6 +41,9 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 
 public class AnalysisServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
 
     @Mock
     private AnalysisRepository analysisRepository;
@@ -50,6 +60,7 @@ public class AnalysisServiceTest {
     private User user;
     private Folder folder;
     private Record record;
+    private Analysis analysis;
 
     private String testTitle = "Test Record";
     private String testContent = "Test".repeat(10);
@@ -60,14 +71,14 @@ public class AnalysisServiceTest {
         user = createMockUser();
         folder = createMockFolder(user);
         record = createMockRecord(user, folder);
+        analysis = createMockAnalysis(record);
+        analysis.setCreatedAt(LocalDateTime.now());
     }
 
     @Test
     @DisplayName("메모 역량 분석 생성 테스트")
     void createMemoAnalysisTest() {
         // Given
-        Analysis analysis = createMockAnalysis(record);
-
         when(openAiService.generateMemoSummary(any(String.class))).thenReturn(testContent);
         when(openAiService.generateAbilityAnalysis(any(String.class)))
                 .thenReturn(new AnalysisAiResponse(Map.of("커뮤니케이션", "Test Keyword Content"), "Test Comment"));
@@ -130,48 +141,105 @@ public class AnalysisServiceTest {
     }
 
     @Test
-    @DisplayName("존재하지 않는 키워드 제시 시 예외 발생 테스트")
-    void findAbilityByNotExistingKeyword() {
+    @DisplayName("역량 분석 수정 테스트")
+    void updateAnalysisTest() {
         // Given
+        Ability ability = createMockAbility(analysis);
+        analysis.addAbility(ability);
 
-        // When & Then
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(analysisRepository.findAnalysisById(1L)).thenReturn(Optional.of(analysis));
 
+        // When
+        AnalysisRequest.AnalysisUpdateDto request = AnalysisRequest.AnalysisUpdateDto.builder()
+                .analysisId(1L)
+                .title("Updated Title")
+                .content("Updated Content".repeat(5))
+                .abilityMap(Map.of("커뮤니케이션", "Updated Keyword Content"))
+                .build();
+
+        AnalysisResponse.AnalysisDto response = analysisService.updateAnalysis(1L, request);
+
+        // Then
+        verify(userRepository, times(1)).findById(1L);
+        verify(analysisRepository, times(1)).findAnalysisById(1L);
+
+        assertEquals(response.getAnalysisId(), analysis.getAnalysisId());
+        assertEquals(response.getRecordId(), record.getRecordId());
+        assertEquals(response.getRecordTitle(), "Updated Title");
+        assertEquals(response.getRecordContent(), "Updated Content".repeat(5));
+        assertEquals(response.getComment(), analysis.getComment());
+        assertEquals(response.getAbilityDtoList().get(0).getKeyword(), "커뮤니케이션");
+        assertEquals(response.getAbilityDtoList().get(0).getContent(), "Updated Keyword Content");
     }
 
-
     @Test
-    @DisplayName("메모 역량 분석 재생성 테스트")
-    void recreateAnalysisTest() {
+    @DisplayName("역량 분석 수정 중 존재하지 않는 키워드 제시 시 예외 발생 테스트")
+    void findAbilityByNotExistingKeyword() {
         // Given
-        // When
-        // Then
+        Ability ability = createMockAbility(analysis);
+        analysis.addAbility(ability);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(analysisRepository.findAnalysisById(1L)).thenReturn(Optional.of(analysis));
+
+        // When & Then
+        AnalysisRequest.AnalysisUpdateDto request = AnalysisRequest.AnalysisUpdateDto.builder()
+                .analysisId(1L)
+                .abilityMap(Map.of("협동", testContent))
+                .build();
+
+        AbilityException exception = assertThrows(AbilityException.class,
+                () -> analysisService.updateAnalysis(1L, request));
+
+        assertEquals(exception.getAbilityErrorStatus(), AbilityErrorStatus.INVALID_KEYWORD);
+        verify(userRepository, times(1)).findById(1L);
+        verify(analysisRepository, times(1)).findAnalysisById(1L);
     }
 
     @Test
     @DisplayName("역량 분석 상세 정보 조회 테스트")
     void getAnalysisDetailTest() {
         // Given
+        Ability ability = createMockAbility(analysis);
+        analysis.addAbility(ability);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(analysisRepository.findAnalysisById(1L)).thenReturn(Optional.of(analysis));
+
         // When
+        AnalysisResponse.AnalysisDto response = analysisService.getAnalysis(1L, 1L);
+
         // Then
+        verify(userRepository, times(1)).findById(1L);
+        verify(analysisRepository, times(1)).findAnalysisById(1L);
 
-    }
-
-    @Test
-    @DisplayName("역량 분석 수정 테스트")
-    void updateAnalysisTest() {
-        // Given
-        // When
-        // Then
-
+        assertEquals(response.getAnalysisId(), analysis.getAnalysisId());
+        assertEquals(response.getRecordId(), record.getRecordId());
+        assertEquals(response.getRecordTitle(), testTitle);
+        assertEquals(response.getRecordContent(), testContent);
+        assertEquals(response.getComment(), testComment);
+        assertEquals(response.getAbilityDtoList().get(0).getKeyword(), "커뮤니케이션");
+        assertEquals(response.getAbilityDtoList().get(0).getContent(), "Test Keyword Content");
     }
 
     @Test
     @DisplayName("역량 분석 삭제 테스트")
     void deleteAnalysisTest() {
         // Given
-        // When
-        // Then
+        Ability ability = createMockAbility(analysis);
+        analysis.addAbility(ability);
 
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(analysisRepository.findAnalysisById(1L)).thenReturn(Optional.of(analysis));
+
+        // When
+        analysisService.deleteAnalysis(1L, 1L);
+
+        // Then
+        verify(userRepository, times(1)).findById(1L);
+        verify(analysisRepository, times(1)).findAnalysisById(1L);
+        verify(analysisRepository).delete(analysis);
     }
 
     private User createMockUser() {
@@ -209,6 +277,7 @@ public class AnalysisServiceTest {
                 .content(testContent)
                 .comment(testComment)
                 .record(record)
+                .abilityList(new ArrayList<>())
                 .build();
     }
 
