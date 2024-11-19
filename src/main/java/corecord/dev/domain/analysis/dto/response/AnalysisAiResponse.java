00ae92f54f66833094e0beeb644664125ef1dad3
daeun084 <030804jk@naver.com>
2024-11-19 18:53:50 +0900

    !hotfix: 역량 분석 response @NoargsConstructor 추가

--- src/main/java/corecord/dev/domain/analysis/dto/response/AnalysisAiResponse.java
+++ src/main/java/corecord/dev/domain/analysis/dto/response/AnalysisAiResponse.java
@@ -1,15 +1,13 @@
 package corecord.dev.domain.analysis.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Map;
 
 @Setter
 @Getter @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class AnalysisAiResponse {
     @JsonProperty("keywordList")
