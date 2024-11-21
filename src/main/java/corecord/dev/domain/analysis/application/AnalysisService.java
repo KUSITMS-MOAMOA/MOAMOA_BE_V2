03ff61c0d083b7362b2d520de36f3e7e0f8de4ec
daeun084 <030804jk@naver.com>
2024-11-21 18:22:35 +0900

    chore: test code 수정

--- src/main/java/corecord/dev/domain/analysis/application/AnalysisService.java
+++ src/main/java/corecord/dev/domain/analysis/application/AnalysisService.java
@@ -21,7 +21,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
 
-@Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class AnalysisService {
