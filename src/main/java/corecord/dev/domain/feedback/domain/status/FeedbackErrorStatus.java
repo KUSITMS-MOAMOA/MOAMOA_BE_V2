f3ceecd35e3efdf39fd38a9c24b264594756182f
oosedus <seayeon0729@gmail.com>
2025-02-18 00:02:19 +0900

    chore : 이미 피드백 작성한 경험에 대한 에러 코드 추가

--- src/main/java/corecord/dev/domain/feedback/domain/status/FeedbackErrorStatus.java
+++ src/main/java/corecord/dev/domain/feedback/domain/status/FeedbackErrorStatus.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum FeedbackErrorStatus implements BaseErrorStatus {
     OVERFLOW_FEEDBACK_COMMENT(HttpStatus.BAD_REQUEST, "E203_OVERFLOW_FEEDBACK_COMMENT", "피드백 내용은 200자 이내여야 합니다."),
     UNAUTHORIZED_FEEDBACK(HttpStatus.UNAUTHORIZED, "E203_FEEDBACK_UNAUTHORIZED", "유저가 해당 피드백에 대한 권한이 없습니다."),
+    ALREADY_FEEDBACK(HttpStatus.BAD_REQUEST, "E203_ALREADY_FEEDBACK", "이미 피드백을 작성한 경험입니다."),
     ;
 
     private final HttpStatus httpStatus;
