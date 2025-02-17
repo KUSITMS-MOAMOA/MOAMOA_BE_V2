d64f814430c501821d9ab9ff5a7788191713bd53
oosedus <seayeon0729@gmail.com>
2025-02-18 00:03:04 +0900

    fix : successStatus에서 타입  httpstatus로 변경

--- src/main/java/corecord/dev/domain/feedback/domain/status/FeedbackSuccessStatus.java
+++ src/main/java/corecord/dev/domain/feedback/domain/status/FeedbackSuccessStatus.java
@@ -4,7 +4,6 @@ import corecord.dev.common.base.BaseSuccessStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 
 @Getter
 @AllArgsConstructor
@@ -12,7 +11,7 @@ public enum FeedbackSuccessStatus implements BaseSuccessStatus {
     FEEDBACK_POST_SUCCESS(HttpStatus.CREATED, "S203", "AI 만족도 조사 결과가 성공적으로 저장되었습니다."),
     ;
 
-    private final HttpStatusCode httpStatus;
+    private final HttpStatus httpStatus;
     private final String code;
     private final String message;
 }
